b4ba4f8ad07eac24f8343c04a315a027462269d9
Matthew Mazankowski <m.g.mazankowski@gmail.com>
2022-02-13 23:56:21 -0700

    first steps
    seeded data
    added repo
    lots more

---
+++ Mission7-Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission7_Bookstore.Models;
+
+namespace Mission7_Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private iBookstoreRepository repo;
+
+        public HomeController(iBookstoreRepository temp)
+        {
+            repo = temp; 
+        }
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+
+        //public HomeController(BookstoreContext temp)
+        //{
+        //    context = temp;
+        //}
+
+        //private BookstoreContext context { get; set; }
+
+        //public HomeController(BookstoreContext temp) => context = temp; 
+
+        public IActionResult Index()
+        {
+            var blah = repo.Books.ToList();
+
+            return View(blah);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
